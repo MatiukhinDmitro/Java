49f0bd318be7333b1774a059c6bc9f2abda9efa0
MatiukhinDmitro <ToxicAngelAC@mail.ru>
2019-11-28 10:51:07 +0200

    Recreate Lab2

--- src/main/java/Lab2/Main.java
+++ src/main/java/Lab2/Main.java
@@ -1,9 +1,9 @@
 package Lab2;
 
-import Lab2.controller.UniversityCreator;
-import Lab2.model.University;
+        import Lab2.controller.UniversityCreator;
+        import Lab2.model.University;
 
-import java.util.Scanner;
+        import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
