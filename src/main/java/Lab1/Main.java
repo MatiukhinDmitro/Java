f0a040eaecef49f9a5a7d099188a164fb3667f34
MatiukhinDmitro <ToxicAngelAC@mail.ru>
2019-11-28 10:34:50 +0200

    Recreate Lab1

--- src/main/java/Lab1/Main.java
+++ src/main/java/Lab1/Main.java
@@ -1,15 +1,15 @@
-package lab1;
+package Lab1;
 
 public class Main {
 
     public static void main(String[] args) {
         MatrixCreator matrixCreator = new MatrixCreator();
-        int[][] array = matrixCreator.enterMatrix();
-        int arrayMax = matrixCreator.discoverMax(array);
-        int arrayMin = matrixCreator.discoverMin(array);
-        int arraySum = matrixCreator.calculateSum(array);
+        int[][] array = MatrixCreator.enterMatrix();
+        int arrayMax = MatrixCreator.discoverMax(array);
+        int arrayMin = MatrixCreator.discoverMin(array);
+        int arraySum = MatrixCreator.calculateSum(array);
 
-        matrixCreator.printMatrix(array);
+        MatrixCreator.printMatrix(array);
         System.out.println("\nMax: " + arrayMax + "\nMin: " + arrayMin + "\nSum: " + arraySum);
     }
 
